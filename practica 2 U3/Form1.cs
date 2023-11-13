59fe7f63faa493c26a1097d5e6728f3d61c47562
Administrador <Administrador@WENDER-PC>
2023-11-13 00:02:32 -0600

    Agregar archivos de proyecto.

---
+++ practica 2 U3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_2_U3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n, count = 0, limite;
+            
+            n = int.Parse(textBox1.Text);
+            limite = int.Parse(textBox2.Text);
+            do
+            {
+                listBox1.Items.Add(n + " * " + count + "=" + n * count);
+                count++;
+            } while (count <= limite);
+ }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int n, count = 0, limite;
+
+            n = int.Parse(textBox1.Text);
+            limite = int.Parse(textBox2.Text);
+            do
+            {
+                listBox1.Items.Add(n + " * " + count + "=" + n * count);
+                count++;
+            } while (count <= limite);
+        }
+    }
+}
+
+
